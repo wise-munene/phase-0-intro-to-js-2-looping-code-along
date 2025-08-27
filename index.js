9bfdd4eadfe79f836a7ccfd71966523cc2c83fbf
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-08-27 22:07:34 +0300

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions in the field below//
+function writeCards(names, event){
+    let messages=[]; // array going to contain the messages
+
+    for (let i=0; i< names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message); // adds the messages to the array
+
+    }
+    return messages;  // all the messages are returned
+    }
+    let cards= writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");  // function call
+    
+    console.log(cards); // showws the result
+
+    //while loop
+    function countDown(number){
+        while(number>=0){
+            console.log(number);
+            number --;
+        }
+    }
